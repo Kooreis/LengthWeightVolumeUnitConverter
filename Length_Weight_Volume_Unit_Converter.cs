96117116d44fcaf3372d69140c917db2e63ba77a
Kooreis <kooreis.official@gmail.com>
2022-11-10 16:00:00 -0600

    Initial setup of the program, including the welcome message and options for the user to choose from.

--- Length_Weight_Volume_Unit_Converter.cs
+++ Length_Weight_Volume_Unit_Converter.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 namespace UnitConverter
@@ -13,29 +12,6 @@ namespace UnitConverter
             Console.WriteLine("3. Volume");
             Console.Write("Choose an option: ");
             int option = Convert.ToInt32(Console.ReadLine());
-
-            switch (option)
-            {
-                case 1:
-                    Console.Write("Enter length in meters: ");
-                    double length = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Length in feet: " + length * 3.28084);
-                    break;
-                case 2:
-                    Console.Write("Enter weight in kilograms: ");
-                    double weight = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Weight in pounds: " + weight * 2.20462);
-                    break;
-                case 3:
-                    Console.Write("Enter volume in liters: ");
-                    double volume = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Volume in gallons: " + volume * 0.264172);
-                    break;
-                default:
-                    Console.WriteLine("Invalid option");
-                    break;
-            }
         }
     }
 }
-```

09fc41a9ce69f09f8aa17fb9ecc711f9b8ef3856
Kooreis <kooreis.official@gmail.com>
2022-11-11 15:00:00 -0600

    Added the first case to the switch statement, which handles the conversion from meters to feet.

--- Length_Weight_Volume_Unit_Converter.cs
+++ Length_Weight_Volume_Unit_Converter.cs
@@ -12,6 +12,15 @@ namespace UnitConverter
             Console.WriteLine("3. Volume");
             Console.Write("Choose an option: ");
             int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.Write("Enter length in meters: ");
+                    double length = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Length in feet: " + length * 3.28084);
+                    break;
+            }
         }
     }
 }
